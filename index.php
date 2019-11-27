605e7341e9f95837388ba00db6f76e8bb2514705
raviMaurya12 <cse170001039@gmail.com>
2019-11-28 03:54:38 +0530

    chept 1

--- index.php
+++ index.php
@@ -1,9 +1,21 @@
 <!DOCTYPE html>
-	<html>
+<html>
 	<head>
-		<title>myApp</title>
+		<title>Process</title>
 	</head>
 	<body bgcolor="cyan">
-			
+		<h1>
+			<center>
+			<?php
+
+				$last_line=system('./test < input.txt');
+				$output = file_get_contents('output.txt');
+				echo $output;
+
+			?>
+			</center>
+		</h1>
+		<hr>
+
 	</body>
 </html>
