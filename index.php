1506c3d16d8dd21ae5eafe1df11a9a29c2f945f4
raviMaurya12 <cse170001039@gmail.com>
2019-11-28 04:25:56 +0530

    inpt7

--- index.php
+++ index.php
@@ -5,23 +5,11 @@
 	</head>
 	<body bgcolor="cyan">
 		<center>
-			<form action="" method="post">
+			<form action="process.php" method="post">
 				<input type="text" name="content" style="font-size:10pt;height:500px;width:1000px;"><br><br>
 				<input type="submit">
 			</form>
 			<p>
-				<?php
-
-					$cont='';
-					if(!empty($_POST["content"])){
-						$cont=$POST["content"];
-					}
-					echo $cont;
-					#$last_line=system('./test < input.txt');
-					#$output = file_get_contents('output.txt');
-					#echo $output;
-
-				?>
 			</p>
 		</center>
 	</body>
