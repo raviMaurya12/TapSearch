3f72b009d395c07a834dfd6f55bff6d63c1aa46f
raviMaurya12 <cse170001039@gmail.com>
2019-11-28 04:12:36 +0530

    inpt7

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 	<body bgcolor="cyan">
 		<center>
 			<form action="index.php" method="post">
-				<input type="text" name="content" class="inputbox"><br><br>
+				<input type="text" name="content" style="font-size:18pt;height:420px;width:200px;"><br><br>
 				<input type="submit">
 			</form>
 			<p>
