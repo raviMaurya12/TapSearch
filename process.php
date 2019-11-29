fdf46b0a239cbd842a0b8daad523fc0ed1bf8051
raviMaurya12 <cse170001039@gmail.com>
2019-11-29 15:52:14 +0530

    hash_implemented

--- process.php
+++ process.php
@@ -7,7 +7,7 @@
 
 			file_put_contents('input.txt', implode(PHP_EOL, $_POST["content"]), FILE_APPEND);
 			$last_line=system('./hash_implementation < input.txt',$retval);
-			$text=nl2br(file_get_contents('input.txt'));
+			$text=nl2br(file_get_contents('output.txt'));
 			echo $text;
 
 		?>
