4f93386299d1f5400c57952b1777f6e62d1bd01d
raviMaurya12 <cse170001039@gmail.com>
2019-11-29 19:08:57 +0530

    trie implemented with interface

--- process.php
+++ process.php
@@ -7,15 +7,13 @@
 		  box-sizing: border-box;
 		}
 
-		/* Create two equal columns that floats next to each other */
 		.column {
 		  float: left;
 		  width: 50%;
 		  padding: 10px;
-		  height: 300px; /* Should be removed. Only for demonstration */
+		  height: auto;
 		}
 
-		/* Clear floats after the columns */
 		.row:after {
 		  content: "";
 		  display: table;
@@ -35,17 +33,27 @@
 		  	<div class="column" style="background-color:#aaa;">
 		    	<h4>Results from HashMap Implementation:</h4>
 		    	<?php 
+		    		$start1 = microtime(true);
 		    		$last_line1=system('./hash_implementation < input.txt',$retval1);
 					$text1=nl2br(file_get_contents('output.txt'));
 					echo $text1;
+					$time_elapsed_secs1 = microtime(true) - $start1;
+					echo "<br><br>";
+					echo "time taken:";
+					echo $time_elapsed_secs1;
 		    	?>
 		  	</div>
 		  	<div class="column" style="background-color:#bbb;">
 		    	<h4>Results from Trie Implementation:</h4>
-		    	<?php 
+		    	<?php
+		    		$start2 = microtime(true); 
 		    		$last_line2=system('./trie_implementation < input.txt',$retval2);
 					$text2=nl2br(file_get_contents('output.txt'));
 					echo $text2;
+					$time_elapsed_secs2 = microtime(true) - $start2;
+					echo "<br><br>";
+					echo "time taken:";
+					echo $time_elapsed_secs2;
 		    	?>
 		  	</div>
 		</div>
