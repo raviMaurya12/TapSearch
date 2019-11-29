be7cf94d7a8203190dd62d73f21d5dfc5047c5de
raviMaurya12 <cse170001039@gmail.com>
2019-11-29 16:04:34 +0530

    hash_implemented

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 			<form action="process.php" method="post">
 				<pre><textarea name="content" style="font-size:8pt;height:450px;width:1000px;"></textarea></pre><br>
 				Enter all words to be searched separated by space:
-				<input type="text" name="searchInput"style="font-size:8pt;width:600px;"><br><br>
+				<input type="text" name="searchInput"style="font-size:8pt;width:660px;"><br><br>
 				<input type="submit">
 			</form>
 		</center>
